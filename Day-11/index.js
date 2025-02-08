885055f417b60af43b48e5588b8aeb93c3f6e87a
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-02-08 13:27:40 +0530

    Updates

--- Day-11/index.js
+++ Day-11/index.js
@@ -1,83 +1,87 @@
-ternary operator
+// ternary operator
 
-condition ? true value1 : false value2
+// condition ? true value1 : false value2
 
-let age = 20;
+// let age = 20;
 
-let isAllowForDl =
-  age > 30 ? "You are allow for dl" : "You are NOT allow for dl.";
-console.log(isAllowForDl, "isAllowForDl");
+// let isAllowForDl =
+//   age > 30 ? "You are allow for dl" : "You are NOT allow for dl.";
+// console.log(isAllowForDl, "isAllowForDl");
 
-let marks = 81;
+// let marks = 81;
 
-let grade = marks >= 90 ? "A" : (marks >= 80 ? (marks >= 83 ? "B+" : "B-") : (marks >= 70 ? "C" : "D" ));
-console.log(grade, "grade");
-
-
-
-
-
-
-do while loop
-
-do {
-
-} while()
-
-let num = 1;
-
-do {
-  console.log(num, "num");
-  num++;
-} while (num > 5);
+// let grade = marks >= 90 ? "A" : (marks >= 80 ? (marks >= 83 ? "B+" : "B-") : (marks >= 70 ? "C" : "D" ));
+// console.log(grade, "grade");
 
 
 
 
 
 
+// do while loop
 
+// do {
 
-// for in
+// } while(conditions)
 
-let data = { name: "Awdiz", age: 10, location: "Vashi" };
+// let num = 1;
 
-for (key in data) {
-  console.log(key, "key", data[key], "value");
-  if (key == "age") {
-    console.log(data[key] >= 18 ? "Allow" : "Not allow");
-  }
-}
-
-
-
-
-
-let data = ["Hi", "Hello", 3];
-
-for (let index in data) {
-  console.log(index, ":", data[index]);
-}
+// do {
+//   console.log(num, "num");
+//   num++;
+// } while (num < 5);
 
 
 
 
-// for of
 
-var data = ["Hi", "Hello", 3];
-for (let num of data) {
-  console.log(num);
-}
 
-function applyOperation(a, b, operation) {
-  return operation(a, b);
-}
 
-const add = (x, y) => x + y;
-const multi = (x, y) => x * y;
 
-console.log(applyOperation(10, 20, add));
-console.log(applyOperation(10, 20, multi));
+// // for in
+
+// let data = { name: "Awdiz", age: 10, location: "Vashi" };
+
+// for (key in data) {
+//   console.log(key, "key", data[key], "value");
+//   if (key == "age") {
+//     console.log(data[key] >= 18 ? "Allow" : "Not allow");
+//   }
+// }
+
+
+
+
+
+// let data = ["Hi", "Hello", 3];
+
+// for (let index in data) {
+//   console.log(index, ":", data[index]);
+// }
+
+
+
+
+// // for of
+
+// var data = ["Hi", "Hello", 3];
+// for (let num of data) {
+//   console.log(num);
+// }
+
+
+
+
+
+// function applyOperation(a, b, operation) {
+//   return operation(a, b);
+// }
+
+// const add = (x, y) => x + y;
+// const multi = (x, y) => x * y;
+
+// console.log(applyOperation(10, 20, add));
+// console.log(applyOperation(10, 20, multi));
 
 
 
@@ -86,7 +90,7 @@ console.log(applyOperation(10, 20, multi));
 const person = {
   name: "Alice",
   greet: () => {
-    console.log(`Hello, Im ${this.name}.`);
+    console.log(`Hello, Im ${this.name}.`)
   },
   regularFunctionGreeet: function () {
     console.log(`Hello, Im ${this.name}.`);
@@ -99,22 +103,24 @@ person.regularFunctionGreeet();
 
 
 
-(function () {
-  console.log("Hello");
-})();
+// (function () {
+//   console.log("Hello");
+// })();
 
 
 
 
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  greet() {
-    return `Hi, Im ${this.name} and my age is ${this.age}`;
-  }
-}
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;// assign
+//     this.age = age;// assign
+//   }
+//   kuchbhi() {
+//     return `Hi, Im ${this.name} and my age is ${this.age}`;
+//   }
 
-const newPerson = new Person("Awdiz", 10);
-console.log(newPerson.greet());
+// }
+
+// // console.log(Person,"Person")
+// const newPerson = new Person("Awdiz", 10);
+// console.log(newPerson);
